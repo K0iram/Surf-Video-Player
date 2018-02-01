5b8528a1925f6dc8ce9703970e7d59149165f6f6
mario7746 <m.kennedykavouras@gmail.com>
2018-01-31 21:36:07 -0800

    Adds check for video id in preview component. Adds hover effect

--- src/components/Preview/index.js
+++ src/components/Preview/index.js
@@ -6,18 +6,23 @@ import './style.css'
 const VideoPreview  = ({ snippet, id }) => {
 	const { title, thumbnails, description } = snippet
 	const { videoId } = id
-  return (
- 		<div className="youtube-container">
-			<figure className="youtube-container__image">
-				<a href={`https://www.youtube.com/watch?v=${videoId}`} target="_blank">
-					<img src={thumbnails.medium.url} alt={description}/>
-				</a>
-			</figure>
-			<div className="youtube-container__description">
-				<p>{title}</p>
-			</div>
- 		</div>
-  )
+
+	if(videoId === undefined) {
+		return null
+	} else {
+	  return (
+	 		<div className="youtube-container">
+				<figure className="youtube-container__image">
+					<a href={`https://www.youtube.com/watch?v=${videoId}`} target="_blank">
+						<img src={thumbnails.medium.url} alt={description}/>
+					</a>
+				</figure>
+				<div className="youtube-container__description">
+					<p>{title}</p>
+				</div>
+	 		</div>
+	 	)
+	}
 }
   
 
